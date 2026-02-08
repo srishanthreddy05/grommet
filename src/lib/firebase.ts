e339fc9f10289ce8e0bcda6f0c089d42af7dd691
Srishanth <srishanthreddyy05@gmail.com>
2026-02-08 17:03:12 +0530

    env file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8ms2shEg2SUPE63tSKWmtazlnizCqykw",
-  authDomain: "grommetxn.firebaseapp.com",
-  databaseURL: "https://grommetxn-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "grommetxn",
-  storageBucket: "grommetxn.firebasestorage.app",
-  messagingSenderId: "962590269970",
-  appId: "1:962590269970:web:16f81ff112daf4d60feecb"
+   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!, // ✅ REQUIRED
+  
 };
 
 // Initialize Firebase (singleton pattern)
